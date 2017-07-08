f2a062db7deb9be9e8a272b37d875c82f0e0b422
Seokjoon Yun <sj@zigbang.com>
2017-07-08 16:31:16 +0900

    change razor to mvc

--- MVC/Startup.cs
+++ MVC/Startup.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace MVC
 {
@@ -23,6 +21,7 @@ namespace MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc();
         }
 
@@ -36,7 +35,7 @@ namespace MVC
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
--- MVC/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ MVC/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 49755,
+    interval: 49814,
     pause: 'hover',
     wrap: true,
     keyboard: true
